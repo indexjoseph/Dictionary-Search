2cdc7a72be1f7b12db234da5d9ed0fa8f74bbaaa
indexjoseph <joladeji03@gmail.com>
2022-12-29 23:58:11 -0500

    Fixed dropdown menu to match width of search bar

--- search/src/app/app.component.ts
+++ search/src/app/app.component.ts
@@ -3,104 +3,109 @@ import { FormControl } from '@angular/forms';
 import { MatSelect } from '@angular/material/select';
 import { ReplaySubject, Subject } from 'rxjs';
 import { take, takeUntil } from 'rxjs/operators';
-import { readFileSync } from 'fs';
+import * as fs from 'fs'
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, OperatorFunction } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { readFileSync, readFile } from 'fs';
 
 const states = [
-	'Alabama',
-	'Alaska',
-	'American Samoa',
-	'Arizona',
-	'Arkansas',
-	'California',
-	'Colorado',
-	'Connecticut',
-	'Delaware',
-	'District Of Columbia',
-	'Federated States Of Micronesia',
-	'Florida',
-	'Georgia',
-	'Guam',
-	'Hawaii',
-	'Idaho',
-	'Illinois',
-	'Indiana',
-	'Iowa',
-	'Kansas',
-	'Kentucky',
-	'Louisiana',
-	'Maine',
-	'Marshall Islands',
-	'Maryland',
-	'Massachusetts',
-	'Michigan',
-	'Minnesota',
-	'Mississippi',
-	'Missouri',
-	'Montana',
-	'Nebraska',
-	'Nevada',
-	'New Hampshire',
-	'New Jersey',
-	'New Mexico',
-	'New York',
-	'North Carolina',
-	'North Dakota',
-	'Northern Mariana Islands',
-	'Ohio',
-	'Oklahoma',
-	'Oregon',
-	'Palau',
-	'Pennsylvania',
-	'Puerto Rico',
-	'Rhode Island',
-	'South Carolina',
-	'South Dakota',
-	'Tennessee',
-	'Texas',
-	'Utah',
-	'Vermont',
-	'Virgin Islands',
-	'Virginia',
-	'Washington',
-	'West Virginia',
-	'Wisconsin',
-	'Wyoming',
+    'Alabama',
+    'Alaska',
+    'American Samoa',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'District Of Columbia',
+    'Federated States Of Micronesia',
+    'Florida',
+    'Georgia',
+    'Guam',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Marshall Islands',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Northern Mariana Islands',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Palau',
+    'Pennsylvania',
+    'Puerto Rico',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virgin Islands',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming',
 ];
 
 interface Website {
-  id: string;
-  name: string;
+    id: string;
+    name: string;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-	// templateUrl: './typeahead-basic.html',
-
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    // templateUrl: './typeahead-basic.html',
+    
 })
 export class AppComponent {
-  title = 'search';
+    title = 'search';
+    
+    public model: any;
+    // text : any = fs.readFile('words.txt', 'utf8', (err, data)=> {
+    //     return data;
+    // });
 
-  public model: any;
-
-	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
-		text$.pipe(
-			debounceTime(200),
-			distinctUntilChanged(),
-			map((term) =>
-				term.length < 2 ? [] : states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
-			),
-		);
-  
-//   public searchInput: string;
-//   public programmingLanguages = [
-//       'Python','TypeScript','C','C++','Java',
-//       'Go','JavaScript','PHP','Ruby','Swift','Kotlin'
-//  ]
-}
+    search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        map((term) =>
+        term.length < 2 ? [] : states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
+        ),
+        );
+        
+        //   public searchInput: string;
+        //   public programmingLanguages = [
+        //       'Python','TypeScript','C','C++','Java',
+        //       'Go','JavaScript','PHP','Ruby','Swift','Kotlin'
+        //  ]
+    }
+    
