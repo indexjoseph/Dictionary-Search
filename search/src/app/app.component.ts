5f7186b0a4fb939ebee8200f245066b520b886ba
indexjoseph <joladeji03@gmail.com>
2022-12-31 23:30:13 -0500

    Fixing search output

--- search/src/app/app.component.ts
+++ search/src/app/app.component.ts
@@ -10,69 +10,12 @@ import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { readFileSync, readFile } from 'fs';
-
-const states = [
-    'Alabama',
-    'Alaska',
-    'American Samoa',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'District Of Columbia',
-    'Federated States Of Micronesia',
-    'Florida',
-    'Georgia',
-    'Guam',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Marshall Islands',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Northern Mariana Islands',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Palau',
-    'Pennsylvania',
-    'Puerto Rico',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virgin Islands',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming',
-];
-
+import { HttpClient } from '@angular/common/http';
+import word from './words.json'
+import { filter } from 'rxjs/internal/operators/filter';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { merge} from 'rxjs';
+const words  = word.word;
 interface Website {
     id: string;
     name: string;
@@ -82,30 +25,28 @@ interface Website {
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    // templateUrl: './typeahead-basic.html',
     
 })
 export class AppComponent {
     title = 'search';
     
     public model: any;
-    // text : any = fs.readFile('words.txt', 'utf8', (err, data)=> {
-    //     return data;
-    // });
+    @ViewChild('instance', { static: true }) instance: NgbTypeahead;
+	focus$ = new Subject<string>();
+	click$ = new Subject<string>();
 
-    search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
-    text$.pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        map((term) =>
-        term.length < 2 ? [] : states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
-        ),
-        );
+	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+		const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+		const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+		const inputFocus$ = this.focus$;
+
+		return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+			map((term) =>
+				(term === '' ? words : words.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10),
+			),
+		);
+	};
         
-        //   public searchInput: string;
-        //   public programmingLanguages = [
-        //       'Python','TypeScript','C','C++','Java',
-        //       'Go','JavaScript','PHP','Ruby','Swift','Kotlin'
-        //  ]
+ 
     }
     
