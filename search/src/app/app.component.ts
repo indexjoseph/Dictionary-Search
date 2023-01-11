c334ac43a38503a017929949f5185e067ae75b95
jaoladeji <joladeji03@gmail.com>
2023-01-10 23:45:11 -0500

    Finished About Component

--- search/src/app/app.component.ts
+++ search/src/app/app.component.ts
@@ -1,43 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
-import { Observable, OperatorFunction, Subject, merge } from 'rxjs';
-import { filter } from 'rxjs/internal/operators/filter';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
-import dictionary from './dictionary.json';
-import { FormsModule, NgForm } from '@angular/forms';
-const words : string[]  = dictionary.words;
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	
 })
 
 export class AppComponent {
 
-itemSelected($event : any) {
-    alert($event.item);
-  }
-
-    title = 'search';
-    public model: any;
-    
-    @ViewChild('instance', { static: true }) instance: NgbTypeahead;
-	focus$ = new Subject<string>();
-	click$ = new Subject<string>();
-	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
-		const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
-		const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
-		const inputFocus$ = this.focus$;
-		return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
-			map((searchedTerm) =>
-				(searchedTerm === '' ? words : words.filter((word : string) => 
-                word.toLowerCase().startsWith(searchedTerm.toLowerCase())
-			    )).slice(0, 9)
-            )
-		);
-	};
-
+	itemSelected($event: any) {
+		alert($event.item);
+	}
+	
 }
-    
--- search/src/app/app.module.ts
+++ search/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ResultComponent } from './result/result.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +26,7 @@ import { AboutComponent } from './about/about.component';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HomeComponent, AboutComponent, ResultComponent]
 })
 
 export class AppModule {}
