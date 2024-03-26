6b1adf096e7a6878f48f8b9ab44e4bd57487d403
Joseph Oladeji <73621296+indexjoseph@users.noreply.github.com>
2024-03-25 23:42:01 -0400

    Update dictionary.service.ts

--- src/app/dictionary.service.ts
+++ src/app/dictionary.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DictionaryService {
-  private api_key: string = "924877a3-f8ff-4be4-8bbe-ab2a8da67232";
+
   constructor(private http: HttpClient) { }
   getData(word: string) {
     let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${this.api_key}`;
